954567db200beb6ae529875971439d432145dcee
Gititdood <yan.ramgoulam@gmail.com>
2016-02-15 17:39:36 -0500

    added come code

--- server/db/index.js
+++ server/db/index.js
@@ -1,7 +1,11 @@
 var redis = require('redis');
 var client = redis.createClient();
 
-client.on('ready', () => console.log('Redis client ready!'));
+client.on('ready', () => {
+	console.log('Redis client ready!')
+	client.select(1,function(err,res){})
+});
+
 client.on('error', err => console.log('Redis client error:', err));
 
 exports.db = client;
