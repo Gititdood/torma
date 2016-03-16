f2ddb321c920533755e33bfe58a9108410664206
laggingreflex <laggingreflex@gmail.com>
2016-03-16 20:29:52 +0530

    dynamic content

--- client/js/login/index.js
+++ client/js/login/index.js
@@ -1,29 +1,28 @@
-
-
-
-$( document ).ready(function() {
-    var working = false;
-	$('.login').on('submit', function(e) {
-	  e.preventDefault();
-	  if (working) return;
-	  working = true;
-	  var $this = $(this),
-		$state = $this.find('button > .state');
-	  $this.addClass('loading');
-	  $state.html('Authenticating');
-	  setTimeout(function() {
-		$this.addClass('ok');
-		$state.html('Welcome back!');
-		setTimeout(function() {
-			var inputform = {};
-			inputform['username'] = $("#username").val();
-			inputform['password'] = $("#password").val();
-			
-		  $.post( "/login", inputform, function( data ) {
-			  alert(data);
-			});
-		}, 1000);
-	  }, 1000);
-	});
+$(document).ready(function() {
+  var working = false;
+  $('.login').on('submit', function(e) {
+    e.preventDefault();
+    if (working) return;
+    working = true;
+    var $this = $(this),
+      $state = $this.find('button > .state');
+    $this.addClass('loading');
+    $state.html('Authenticating');
+    var inputform = {};
+    inputform['username'] = $("[name=username]").val();
+    inputform['password'] = $("[name=password]").val();
+    $.post('/login', inputform)
+      .done(function() {
+        $this.addClass('ok');
+        $state.html('Welcome back!');
+        setTimeout(function() {
+          window.location = '/';
+        }, 1000);
+      })
+      .fail(function() {
+        $this.removeClass('loading');
+        $this.addClass('error');
+        $state.html('Oops, login failed!');
+      });
+  });
 });
-
